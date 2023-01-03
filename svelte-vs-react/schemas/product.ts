6bedd1900e42f16ee7fe42d359f56b6345f7fa2c
Martin Myhre <70258183+NotLimey@users.noreply.github.com>
2023-01-03 11:55:02 +0100

    product

---
+++ svelte-vs-react/schemas/product.ts
@@ -0,0 +1,32 @@
+import { defineType } from "sanity";
+import { ShoppingBagIcon } from "@heroicons/react/24/outline"
+
+export const product = defineType({
+    name: "product",
+    title: "Product",
+    type: "document",
+    icon: ShoppingBagIcon,
+    fields: [
+        {
+            name: "name",
+            title: "Name",
+            type: "string",
+        },
+        {
+            name: "description",
+            title: "Description",
+            type: "text",
+        },
+        {
+            name: "price",
+            title: "Price",
+            type: "number",
+            validation: (Rule) => Rule.min(0).max(10000),
+        },
+        {
+            name: "image",
+            title: "Image",
+            type: "image",
+        }
+    ]
+})
